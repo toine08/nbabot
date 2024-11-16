9987eb6c629dbd00b1413bc14257009aaee13c2e
toine08 <riedotoine@gmail.com>
2024-11-16 13:16:13 +0100

    refactor main.py to integrate Game class and improve scoreboard handling

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,22 @@
+from datetime import datetime, timezone, timedelta, date
+from dateutil import parser
+from game import Game
 from nba_api.live.nba.endpoints import scoreboard
+#from nba_api.stats.endpoints import scoreboard as tomorrow_score # Correct endpoint for querying future dates
 
 # Today's Score Board
-games = scoreboard.ScoreBoard()
+#board = scoreboard.ScoreBoard()
+game = Game()
+#game.print_last_score()
+print(game.return_json())
 
 
-# json
-#print(games.get_json())
+###
+# 
+# 
+# 
+# print("ScoreBoardDate: " + board.score_board_date)
 
-# dictionary
-games = games.get_dict()
-team_name = games['scoreboard']['games'][0]['homeTeam']['teamName']
-team_city = games['scoreboard']['games'][0]['homeTeam']['teamCity']
-team_score = games['scoreboard']['games'][0]['homeTeam']['score']
 
-for games in games['scoreboard']['games']:
-    print(games['homeTeam'])
+###
 #print(games['scoreboard']['games'][0]['homeTeam']['score'])
